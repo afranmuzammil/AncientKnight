699ecadc244a852f21b2878f491cb8a63e188b81
shaik muzammil <52369523+afranmuzammil@users.noreply.github.com>
2020-12-02 23:01:08 +0530

    UI setup
    Initail UI set up and Scene management

---
+++ Assets/Scripts/GameContoller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameContoller : MonoBehaviour
+{
+
+    public GameObject OpsMenu;
+    public GameObject MainMenu;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnClickOptions()
+    {
+        OpsMenu.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+    public void OnClickBack()
+    {
+        OpsMenu.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/L1COntoller.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class L1COntoller : MonoBehaviour
+{
+    public GameObject PauseMenu; 
+    public GameObject OpsMenu;
+    public int ScenceNumber;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPressPause()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void OnPressPlay()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void OnPressRestart()
+    {
+        SceneManager.LoadScene(ScenceNumber);
+        OnPressPlay();
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void OnPressOps()
+    { 
+        PauseMenu.SetActive(false);
+        OpsMenu.SetActive(true);
+       
+    }
+    public void OnClickBack()
+    {
+        OpsMenu.SetActive(false);
+        PauseMenu.SetActive(true);
+    }
+
+
+
+
+}
